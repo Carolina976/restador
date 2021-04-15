3ca32197e225c3cb3e7b714453d5a98ac69dc4b8
Carolina976 <karolina210598@gmail.com>
2021-04-14 22:44:23 -0500

    hola

---
+++ android/app/src/main/kotlin/com/example/hol_mundo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hol_mundo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hol_mundo/src/pages/home_page.dart';
+
+void main() {
+  runApp(HomePage());
+}
